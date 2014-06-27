b77bc511fef497f91f4e2d106641a2cef8a1b2b5
Anton Buslavskii <buslavskii@gmail.com>
2014-06-27 04:27:57 +0400

    This is the first DB creation script

---
+++ sql/dbCreationScript.sql
@@ -0,0 +1,11 @@
+CREATE TABLE sensors (
+Id bigint unsigned unique auto_increment PRIMARY KEY,
+sensorName varchar(1024)
+);
+
+CREATE TABLE sensorValues (
+sensorId bigint unsigned, 
+valueDateTime datetime default null,
+valueFigure double,
+FOREIGN KEY (sensorId) references sensors(Id)
+)

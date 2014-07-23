0854a8ab37ae07eabc9e7a7cbaeda264d1bcbdac
Alexey Pershin <alexey@Alexeys-MacBook-Pro.local>
2014-07-23 15:50:17 +0400

    initial iOS project

---
+++ src/prof/ios/SensorsView/SensorsView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SensorsView
+//
+//  Created by Alexey Pershin on 23/07/14.
+//  Copyright (c) 2014 com.tcontrol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

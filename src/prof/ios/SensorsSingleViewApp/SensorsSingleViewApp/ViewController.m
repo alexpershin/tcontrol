e085dd13af2893626d2caf59f58e7fbe80fe62ea
Alexey Pershin <alexey@Alexeys-MacBook-Pro.local>
2014-07-23 18:24:16 +0400

    Orientation handler example

--- src/prof/ios/SensorsSingleViewApp/SensorsSingleViewApp/ViewController.m
+++ src/prof/ios/SensorsSingleViewApp/SensorsSingleViewApp/ViewController.m
@@ -19,10 +19,46 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
+    //Add image
     UIImage *uiimage=[UIImage imageNamed:@"patterns.jpg"];
     UIImageView *view=[[UIImageView alloc] initWithImage:uiimage];
     [self.view addSubview:view];
+    
+    //Add orientation notification
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                               selector:@selector(orientationChanged)
+                                                 name:UIDeviceOrientationDidChangeNotification
+                                               object:nil];
+   
+    
 }
+
+-(void) orientationChanged{
+    switch([[UIDevice currentDevice] orientation]){
+        case UIDeviceOrientationPortrait:
+            NSLog(@">>>Portrait");
+            break;
+            case UIDeviceOrientationPortraitUpsideDown:
+            NSLog(@">>>PortraitUpsideDown");
+            break;
+        case UIDeviceOrientationLandscapeLeft:
+            NSLog(@">>>LandscapeLeft");
+            break;
+        case UIDeviceOrientationLandscapeRight:
+            NSLog(@">>>LandscapeRight");
+            break;
+        case UIDeviceOrientationFaceDown:
+            NSLog(@">>>FaceDown");
+            break;
+        case UIDeviceOrientationFaceUp:
+            NSLog(@">>>FaceUp");
+            break;
+        case UIDeviceOrientationUnknown:
+            NSLog(@">>>Unknown");
+            break;
+    }
+}
+
 
 - (void)didReceiveMemoryWarning
 {

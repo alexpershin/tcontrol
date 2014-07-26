a2b22e5c56089ae056e010aa380229d4fe8475ab
alexey <alexey@alexeys-mbp>
2014-07-26 19:43:56 +0400

    rest web service java client test

---
+++ src/prof/rest-ws-java-client/src/main/java/com/tcontrol/prof/restws/client/ClientConsoleTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tcontrol.prof.restws.client;
+
+import com.tcontrol.prof.Sensor;
+
+/**
+ *
+ * @author alexey
+ */
+public class ClientConsoleTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JerseySensorsClient client = new JerseySensorsClient();
+        try {
+            String result =//client.getSensors().toString();
+                    client.getStatus();
+            System.out.println("Status=" + result);
+            System.out.print("Sensors:");
+            for(Sensor sensor:client.getSensors()){
+                System.out.print(sensor+";");
+            }
+            System.out.println();
+            
+        } finally {
+            client.close();
+        }
+
+    }
+
+}
---
+++ src/prof/rest-ws-java-client/src/main/java/com/tcontrol/prof/restws/client/JerseySensorsClient.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tcontrol.prof.restws.client;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.tcontrol.prof.Sensor;
+import java.util.List;
+
+/**
+ * @author alexey
+ */
+public class JerseySensorsClient {
+    private WebResource webTarget;
+    private Client client;
+    private static final String BASE_URI = 
+            "http://localhost:8080/rest-ws-web/webresources";
+
+    public JerseySensorsClient() {
+        ClientConfig clientConfig = new DefaultClientConfig();
+
+			clientConfig.getFeatures().put(
+					JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+        client = Client.create(clientConfig);
+        webTarget = client.resource(BASE_URI);
+    }
+
+    public List<Sensor>  getSensors() {
+        WebResource resource = webTarget;
+        List<Sensor> sensors = resource.path("sensors").get(new GenericType<List<Sensor>>(){});
+        return sensors;   
+    }
+
+    public String getStatus() {
+        WebResource resource = webTarget;
+        return resource.path("status").get(String.class);
+    }
+
+    public void close() {
+        client.destroy();
+    }
+    
+}

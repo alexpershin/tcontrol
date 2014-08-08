02fbf945643fe73b8783b2cf9412617a90a00882
Anton <Anton@Anton-PC.dfpost.ru>
2014-08-08 05:10:49 -0700

    Changes of methods, making some of them void

--- src/dao-test/dao/src/main/java/com/tcontrol/dao/dao/daoInterface.java
+++ src/dao-test/dao/src/main/java/com/tcontrol/dao/dao/daoInterface.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.tcontrol.dao.dao;
 
-import java.sql.Time;
+//import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -16,17 +16,27 @@ import java.util.Date;
  */
 public interface daoInterface {
 //    TODO: correlate all input parameters and all parameters being returned
-ArrayList<Integer> getSensors(int userId, int[] sensorsIdList); //returns all sensors for the specified user
-ArrayList<String> getValues(int userId, int[] SensorIdList, Date FromPeriod, Date UpToPeriod); // returns all values for listed sensor on specified period of time
-ArrayList<String> addValues(int sensorId, ArrayList<Double> Values); //add values with specified ID, TODO: add datetime to each value
-ArrayList<String> addSensor(int Sensor);// TODO: int is not good here, we should specify the type of the added sensor 
-boolean addSensorToUser(int sensorId,int userId); //true if the sensor is now related to user with specified ID
-boolean removeSensorFromUser(int sensorId); //true if the sensor is now removed from the user with specified ID and NOT related to any user
-boolean checkFreedomOfSensor(int sensorId); //true if the sensor is now NOT related to any
-int checkReferenceOfSensorToUser(int sensorId); //returns int Id of the user the sensor is related to
+
+    SensorValue[] getSensors(int userId); //returns all sensors ID for the specified user. We get userId and return all user's sensors
+
+    List<SensorValue> getCurrentValues(int userId, int[] sensorIds); //returns list of SensorValues
+//TODO: realize this method: List<SensorValue> getValues(int userId, int[] SensorIdList, Date FromPeriod, Date UpToPeriod); // returns all values for listed sensor on specified period of time
+
+    void addValues(int sensorId, ArrayList<Double> Values); //add values with specified ID, TODO: add datetime to each value
+
+    void addSensor(String SensorType);// we specify the type of the added sensor as it name
+
+    boolean addSensorToUser(int sensorId, int userId); //true if the sensor is now related to user with specified ID
+
+    void removeSensorFromUser(int sensorId); //true if the sensor is now removed from the user with specified ID and NOT related to any user
+
+    List<Integer> getSensorsLinkedToUser(int userId); 
+    
+    boolean checkFreedomOfSensor(int sensorId); //true if the sensor is now NOT related to any
+
+    int checkReferenceOfSensorToUser(int sensorId); //returns int Id of the user the sensor is related to
 
 //int newUserAssignment(String userLogin, String userPassword, String userName, String userSurname, String userRole); //assign a new user in a DB, returns an Id of newly added user, by default the role is user
-
 //boolean userDeletion(int userId); //true if the user is marked as deleted and all his/her sensors are not related to this user anymore
 //TODO: Add getters and setters for 
 }

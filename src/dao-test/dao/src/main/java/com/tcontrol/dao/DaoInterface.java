c209ce74ad8bf74ac0dfcb0cb4aa7e824c0e56a2
alexey <alexey@172.27.105.47>
2014-08-12 11:49:21 +0400

    move dao to correct package

--- src/dao-test/dao/src/main/java/com/tcontrol/dao/DaoInterface.java
+++ src/dao-test/dao/src/main/java/com/tcontrol/dao/DaoInterface.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.tcontrol.dao.dao;
+package com.tcontrol.dao;
 
+import com.tcontrol.dao.User;
 import java.util.ArrayList;
 import java.util.List;
 

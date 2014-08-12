9e6e398e47b635fac64950321443fd523779985f
alexey <alexey@172.27.105.47>
2014-08-12 10:28:28 +0400

    implement fields of User

--- src/dao-test/dao/src/main/java/com/tcontrol/dao/dao/User.java
+++ src/dao-test/dao/src/main/java/com/tcontrol/dao/dao/User.java
@@ -10,12 +10,54 @@ package com.tcontrol.dao.dao;
  *
  * @author Anton Buslavskii
  */
-public interface User {
-   
- //TODO: String getName();
- //TODO: String setName();
-    
- //TODO:  int getId();
- //TODO:  int setId();
+public class User {
+   private int id;
+   private String name;
+   private String surname;
+   private int role;
 
+    public User() {
+    }
+
+   
+    public User(int id, String name, String surname, int role) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.role = role;
+    }
+   
+   
+   
+   public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
 }

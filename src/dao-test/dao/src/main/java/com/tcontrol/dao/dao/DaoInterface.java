d197b9b2254b334058de8b80db731763767a722b
alexey <alexey@172.27.105.47>
2014-08-12 11:45:16 +0400

    change DAO methods to manipulate users

--- src/dao-test/dao/src/main/java/com/tcontrol/dao/dao/DaoInterface.java
+++ src/dao-test/dao/src/main/java/com/tcontrol/dao/dao/DaoInterface.java
@@ -5,9 +5,7 @@
  */
 package com.tcontrol.dao.dao;
 
-//import java.sql.Time;
 import java.util.ArrayList;
-//import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,56 +15,63 @@ import java.util.List;
 public interface DaoInterface {
 //    TODO: correlate all input parameters and all parameters being returned
 
-    
     /**
      * Get all sensors in the system.
+     *
      * @return list of identifiers;
      */
-    List<Sensor> getSensors(); 
-    
+    List<Sensor> getSensors();
+
     /**
      * Get sensors by identifiers.
+     *
      * @param ids
      * @return list of sensors;
      */
-    List<Sensor> getSensors(List<Integer> ids); 
+    List<Sensor> getSensors(List<Integer> ids);
 
     /**
      * Returns last(current) values for sensors belong to specified user.
-     * @param userId the user which sensor values to be extracted from the store;
+     *
+     * @param userId the user which sensor values to be extracted from the
+     * store;
      * @return list of sensor values;
      */
-    List<SensorValue> getCurrentValues(int userId); 
-    
-    /** Adds values to sensor with specified ID
+    List<SensorValue> getCurrentValues(int userId);
+
+    /**
+     * Adds values to sensor with specified ID
      *
      * @param sensorId
      * @param values
      */
-    void addValues(int sensorId, ArrayList<SensorValue> values); 
+    void addValues(int sensorId, ArrayList<SensorValue> values);
 
-    /** Adds a new sensor of specified type
+    /**
+     * Adds a new sensor of specified type
      *
      * @param sensorType type of sensor to be added;
      * @return return sensorId of new sensor, NULL otherwise;
      */
-    Integer addSensor(String sensorType);// we specify the type of the added sensor as it name 
+    Integer addSensor(String sensorType);// we specify the type of the added sensor as it name
 
-    
     /**
      * Saves sensor data to storage;
-     * @param sensor 
+     *
+     * @param sensor
      */
     void saveSensor(Sensor sensor);
-    
+
     /**
      * Adds the specified sensor to specified user
+     *
      * @param sensorId
      * @param userId
      */
-    void addSensorToUser(int sensorId, int userId); 
+    void addSensorToUser(int sensorId, int userId);
 
-    /** Removes the sensor with specified ID from user with specified ID
+    /**
+     * Removes the sensor with specified ID from user with specified ID
      *
      * @param userId
      * @param sensorId
@@ -78,37 +83,28 @@ public interface DaoInterface {
      * @param userId
      * @return Returns a list of sensors IDs when gets the user ID as integer
      */
-    List<Integer> getSensorIdsLinkedToUser(int userId); 
-    
-    //boolean checkFreedomOfSensor(int sensorId); //true if the sensor is now NOT related to any
+    List<Integer> getSensorIdsLinkedToUser(int userId);
 
-    //int checkReferenceOfSensorToUser(int sensorId); //returns int Id of the user the sensor is related to
-
-
-    
-    /** Adds a new user with default role
+    /**
+     * Adds a new user
      *
-     * @param userLogin
-     * @param userPassword
-     * @return The userId (integer) of recently created user
+     * @param user new user to be added;
      */
-    int addUser (String userLogin, String userPassword);
-    
-    void editUser (int userId, String userPassword);
-    
-    void editUser (int userId, String userPassword, String userName);
-    
-    void editUser (int userId, String userPassword, String userName, String userSurname);
-    
-    void editUser (int userId, String userPassword, String userName, String userSurname, int userRole);
-    
-    
-     
-   /** Removes the specified user with given ID. All sensors were related with this user become unlinked. 
+    void addUser(User user);
+
+    /**
+     * Update the user data.
+     *
+     * @param user the user to be updated
+     */
+    void updateUser(User user);
+
+    /**
+     * Removes the specified user with given ID. All sensors were related with
+     * this user become unlinked.
      *
      * @param userId
      */
-    void removeUser (int userId);
-    
-    //TODO: Add getters and setters for 
+    void removeUser(int userId);
+
 }

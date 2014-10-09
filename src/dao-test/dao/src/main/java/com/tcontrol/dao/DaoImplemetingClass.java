a0eb6012735da60beb6a5e6859309b8e492e71a3
alexey <alexey@172.27.105.44>
2014-10-09 17:35:30 +0400

    Comment TODO

--- src/dao-test/dao/src/main/java/com/tcontrol/dao/DaoImplemetingClass.java
+++ src/dao-test/dao/src/main/java/com/tcontrol/dao/DaoImplemetingClass.java
@@ -16,7 +16,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * TODO remove this class, all logic should be moved to MySqlJDBCDaoImpl
  * @author Anton Buslavskii
  */
 public class DaoImplemetingClass implements DaoInterface {

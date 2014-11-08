9c36bf8c070120d659749533fdeeeb3d486b4806
alexey <alexey@172.27.105.16>
2014-11-08 22:04:21 +0400

    sensor type processing initial added

--- src/tcontrol/tcontrol-web/src/main/webapp/js/sensors.js
+++ src/tcontrol/tcontrol-web/src/main/webapp/js/sensors.js
@@ -1,9 +1,17 @@
+function sensorsJsonToMap(sensorsJsonData) {
+    result = new Map();
+    $(sensorsJsonData).each(function(key, value) {
+        result.set(value.id, value);
+    });
+    return result;
+}
+
 function layout_sensors(sensorsJsonData) {
     $(sensorsJsonData).each(function(key, value) {
-        clone = $("#sensor_element").clone();
-        clone.appendTo(".sensor_items");
+        clone = $('#sensor_element').clone();
+        clone.appendTo('.sensor_items');
 
-        sensorElementId = clone.attr("id") + value.id;
+        sensorElementId = clone.attr('id') + value.id;
         clone.attr("id", sensorElementId);
 
         sensorTitle =
@@ -14,11 +22,26 @@ function layout_sensors(sensorsJsonData) {
     });
 }
 
-function append_values(sensorsJsonData, valuesJsonData) {
+function append_values(sensorsMap, valuesJsonData) {
     $(valuesJsonData).each(function(key, value) {
-        sensorElementId = "#sensor_element" + value.sensorId;
-        sensor = $(sensorElementId);
-        $(sensorElementId + ' .sensor_item_body').text(value.value);
+        if (sensorsMap.has(value.sensorId)) {
+            sensor = sensorsMap.get(value.sensorId);
+            sensorElementId = '#sensor_element' + value.sensorId;
+            sensorElement = $(sensorElementId);
+            var resValue = '?';
+            if (sensor.type === 'TEMPERATURE') {
+                resValue = value.value + '\xB0';
+            } else if (sensor.type === 'VOLTAGE') {
+                resValue = value.value + ' V';
+            } else if (sensor.type === 'ON_OFF') {
+                if (value.value === 0) {
+                    resValue = 'On';
+                } else if (value.value === 1) {
+                    resValue = 'Off';
+                }
+            }
+            $(sensorElementId + ' .sensor_item_body .sensor_value').text(resValue);
+        }
     });
 }
 

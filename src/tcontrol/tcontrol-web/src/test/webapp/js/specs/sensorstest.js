7f68168a3b56407818cf091efcfcf90014cb0004
alexey <alexey@172.27.105.41>
2014-12-02 18:40:05 +0400

    reorganize JS tests to add testing ability for html logic and rendering part

--- src/tcontrol/tcontrol-web/src/test/webapp/js/specs/sensorstest.js
+++ src/tcontrol/tcontrol-web/src/test/webapp/js/specs/sensorstest.js
@@ -3,24 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-describe("simple test", function() {
-  var a;
-
-  it("and so is a spec", function() {
-    a = true;
-
-    expect(a).toBe(true);
-  });
-});
-
-
-describe("sensors test", function() {
-  var a;
-
-  it("state background calculation", function() {
+describe("sensors logic test", function() {
+    
+ it("state background calculation", function() {
     r=sensorBackgroundCalc({sensorId: 1, value: 25.5, state: 'NORMAL'})
 
     expect(r).toBe(STATE_BACKGROUND.get('NORMAL'));
   });
   
 });
+
+
+describe("sensors render test", function() {
+  
+  beforeEach( function(){
+    //fixture will be reset before each test
+    jasmine.getFixtures()
+        .fixturesPath = 'src/main/webapp/';
+    loadFixtures( 'sensors-body.html');
+  } );
+  
+  it("voltage renderer test", function() {
+      clone = $('#sensor_element').clone();
+      expect(clone.attr('id')).toBe('sensor_element');
+      //voltageSensorRenderer
+  });
+  
+});
+

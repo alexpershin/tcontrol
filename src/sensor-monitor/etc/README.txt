c7150447c253a31ddaed135f34764a15a1a4e998
alexey <alexey@172.20.10.3>
2017-04-10 18:51:08 +0300

    restructurization

--- src/sensor-monitor/etc/README.txt
+++
@@ -1,3 +0,0 @@
-Move application.properties to root class path folder:
-For Glassfish: 'glassfish-root'/glassfish/domains/domain1/config
-For Weblogic: 'weblogic-root'/domains/mydomain/
--- src/sensor-monitor/src/main/java/com/tcontrol/sensormonitor/AlarmListener.java
+++
@@ -1,9 +0,0 @@
-package com.tcontrol.sensormonitor;
-import com.tcontrol.dao.SensorValue;
-/**
- *
- * @author alexey
- */
-public interface AlarmListener {
-    void alarm(SensorValue v);
-}

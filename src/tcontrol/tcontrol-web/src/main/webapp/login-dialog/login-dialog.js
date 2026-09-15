3be866112d3e7ce70ece7acc891d748be7e51dd5
alexpershin <alex.pershin.v@gmail.com>
2026-09-16 01:09:34 +0300

    use RAS-OAEP to encode transfered password

--- src/tcontrol/tcontrol-web/src/main/webapp/login-dialog/login-dialog.js
+++ src/tcontrol/tcontrol-web/src/main/webapp/login-dialog/login-dialog.js
@@ -19,8 +19,52 @@ form.addEventListener('submit', function(e) {
   handleLogin(e, user, pass)
 });
 
+function arrayBufferToBase64Sync(buffer) {
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+  const len = bytes.byteLength;
+
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+
+  return window.btoa(binary);
+}
+
 async function handleLogin(e, user, pass) {
   e.preventDefault();
+
+  const preSignInResponse = await fetch(window.location.protocol
+                                         + '//' + window.location.host
+                                         + "/tcontrol/auth/pre-sign-in?login="+user,
+                                         { method: "POST"});
+
+  const publicKeyBytes = await preSignInResponse.bytes();
+  const keyBuffer = new Uint8Array(publicKeyBytes);
+
+  console.log("public key:")
+  console.log(keyBuffer)
+
+  const publicKey = await window.crypto.subtle.importKey(
+      "spki",
+      keyBuffer,
+      {
+        name: "RSA-OAEP",
+        hash: { name: "SHA-256" } // Secure hashing choice
+      },
+      false, // The key cannot be extracted back out
+      ["encrypt"]
+  );
+
+  const encodedPass = new TextEncoder().encode(pass);
+
+  const encryptedBuffer = await window.crypto.subtle.encrypt(
+       { name: "RSA-OAEP" },
+       publicKey,
+       encodedPass
+  );
+
+  base64Password = arrayBufferToBase64Sync(encryptedBuffer)
 
   // 1. Perform your authentication API call
   const response = await fetch(window.location.protocol
@@ -31,7 +75,7 @@ async function handleLogin(e, user, pass) {
                                                "Content-Type": "application/json", // Tells the server you are sending JSON
                                                "Accept": "application/json"        // Tells the server you expect JSON back
                                              },
-                                       body: JSON.stringify({login: user, password: pass})
+                                       body: JSON.stringify({login: user, password: base64Password})
                                });
   const data = await response.json();
 
